29f77925634813e4672ac5e6e9dae237342f51ae
Zoey Kariyal <zoey.kariyal@stu.ocsb.ca>
2025-04-01 14:42:30 +0000

    newly added area of triangle files

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates area and perimeter of triangle.
+ */
+function calculate () {
+  // input
+  let Height = parseInt(document.getElementById('height-of-triangle').value);
+  let Base = parseInt(document.getElementById('base-of-triangle').value);
+  // process
+  let area = (Base * Height) * 0.5;
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+}
